4ec1587eab31246ef31440318d95909cba0973eb
Michael Buono <michaelbuono5@gmail.com>
2024-02-09 13:09:35 -0600

    Working locally

--- backend/shot_diet/app/__init__.py
+++ backend/shot_diet/app/__init__.py
@@ -27,6 +27,7 @@ app = Flask(__name__)
 logger = logging.getLogger(__name__)
 
 environment = os.getenv("FLASK_ENV", "development")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # Cloud SQL in production, local sqlite in dev
 if environment == "production":
@@ -34,22 +35,35 @@ if environment == "production":
 
     app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://"
     app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"creator": getconn}
+    # Create the SQLAlchemy database object
+    db.init_app(app)
+    migrate = Migrate(app, db)
 else:
     logger.info("Setting up development database")
+    # app.config["SQLALCHEMY_DATABASE_URI"] = (
+    #     "mysql+pymysql://root@127.0.0.1/moneybasketball_test?charset=utf8mb4"
+    # )
     app.config["SQLALCHEMY_DATABASE_URI"] = (
-        "mysql+pymysql://root@127.0.0.1/moneybasketball_test?charset=utf8mb4"
+        "mysql+pymysql://moneybasketball:currywaydowntownbang@db/moneybasketball_test?charset=utf8mb4"
     )
+    # Create the SQLAlchemy database object
+    db.init_app(app)
+    from shot_diet.app.models import Player  # noqa: E402, F401
 
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# Create the SQLAlchemy database object
-db.init_app(app)
+    migrate = Migrate(app, db)
+    with app.app_context():
+        db.create_all()
+
+
+# # Create the SQLAlchemy database object
+# db.init_app(app)
 from .routes import bp  # noqa: E402
 
 app.register_blueprint(bp)  # noqa: F401
 
 
-migrate = Migrate(app, db)
+# migrate = Migrate(app, db)
 app.extensions["sqlalchemy"] = db
 
 # For flask migrate to pick up our models
